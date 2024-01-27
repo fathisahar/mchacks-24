953a5d41fe8a6223b387405e6aabc8d11a78349e
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2024-01-27 15:15:22 -0500

    Setting up firebase
    Also created a gradient for the login page

--- app/App.js
+++ app/App.js
@@ -1,21 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCn-fidJWhdmI9ClD3W2RxaS1BCqS3Irqc",
+  authDomain: "mchacks24-salianmes.firebaseapp.com",
+  projectId: "mchacks24-salianmes",
+  storageBucket: "mchacks24-salianmes.appspot.com",
+  messagingSenderId: "278107575648",
+  appId: "1:278107575648:web:7971f2649fb066ca6b7a05",
+  measurementId: "G-CW8WYVDBT1"
+};
 
 export default function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      if (initializing) {
+        setInitializing(false);
+      }
+    });
+
+    return unsubscribe; // Cleanup on component unmount
+  }, [auth, initializing]); // Include auth and initializing in the dependency array
+
+  const handleLogin = async () => {
+    try {
+      // Replace with your authentication logic (e.g., email/password login)
+      const email = "juliabgrenier.14.5@gmail.com";
+      const password = "julia1234";
+
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error("Login failed:", error.message);
+    }
+  };
+
+  if (initializing) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (!user) {
+    return (
+    <LinearGradient
+      colors={['#E29062', '#DA4167']}
+      style={styles.linearGradient}>
+
+      <View style={styles.container}>
+        <Text>Login Screen</Text>
+        <Button title="Login" onPress={handleLogin} />
+      </View>
+
+    </LinearGradient>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome, {user.email}!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
   },
 });
