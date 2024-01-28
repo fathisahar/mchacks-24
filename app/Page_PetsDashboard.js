7868922e66ac66471fd727c29662dfe0881a7029
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2024-01-28 02:55:00 -0500

    Add icons png

--- app/Page_PetsDashboard.js
+++ app/Page_PetsDashboard.js
@@ -3,6 +3,7 @@ import { Text, Pressable, StyleSheet, View, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import Linear from 'react-native-linear-gradient';
+import { MultipleSelectList } from 'react-native-dropdown-select-list'
 
 import styles from './styles/styles';
 
@@ -22,6 +23,16 @@ const CustomButton = ({ title, destination }) => {
 
 const PetsDashboard = () => {
     const [isModalVisible, setModalVisible] = useState(false);
+    const [selected, setSelected] = React.useState([]);
+    const data = [
+          {key:'1', value:'Mobiles', disabled:true},
+          {key:'2', value:'Appliances'},
+          {key:'3', value:'Cameras'},
+          {key:'4', value:'Computers', disabled:true},
+          {key:'5', value:'Vegetables'},
+          {key:'6', value:'Diary Products'},
+          {key:'7', value:'Drinks'},
+  ]
 
     const toggleFilter = () => {
         setModalVisible(!isModalVisible);
@@ -32,7 +43,15 @@ const PetsDashboard = () => {
             <Text style={styles.headerText}>Spawk Pets{'\n'}Dashboard</Text>
             <Pressable style={styles.filters_button} onPress={toggleFilter}>
                 <Text style={styles.filters_text}>Filters</Text>
+
             </Pressable>
+            <MultipleSelectList
+                    setSelected={(val) => setSelected(val)}
+                    data={data}
+                    save="value"
+                    onSelect={() => alert(selected)}
+                    label="Categories"
+                />
             <TextInput
                 style={[styles.pet_display, { backgroundColor: '#D9D9D9', color: "#FAEFF1" }]}
                 placeholder="Password"
