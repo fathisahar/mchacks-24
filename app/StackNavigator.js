07528f02fa2893dccb420e9b574c80ea0f5ba76c
NancyGong03 <nancy.gong@mail.mcgill.ca>
2024-01-28 05:21:49 -0500

    ProfilePet Page

--- app/StackNavigator.js
+++ app/StackNavigator.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Page_SignUp from './Page_SignUp.js';
 import Page_LogIn from "./Page_LogIn.js";
 import Page_Home from "./Page_Home2.js";
+import Page_ProfilePet from "./Page_ProfilePet.js";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ const StackNavigator = () => {
             <Stack.Screen name="Home" component={Page_Home}/>
             <Stack.Screen name="SignUp" component={Page_SignUp} />
             <Stack.Screen name="LogIn" component={Page_LogIn} />
+            <Stack.Screen name="ProfilePet" component={Page_ProfilePet} />
             </Stack.Group>
         </Stack.Navigator>
     )
--- app/styles/styles.js
+++ app/styles/styles.js
@@ -9,10 +9,144 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container_profile: {
+    flex: 1,
+    backgroundColor: '#DA4167',
+  },
+
+  backgroundBox_profile: {
+    width: '100%',
+    height: 650, 
+    backgroundColor: '#FAEFF1',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    position: 'absolute',
+    bottom: 0,
+  },
+  foregroundBox_profile: {
+    width: '100%', 
+    height: '30%',
+    backgroundColor: '#FAEFF1',
+    position: 'absolute',
+    top: '17%', 
+    left: '0%', 
+    bottom: '40%',
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 7,
+  },
+
+  container2_profile: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 320,
+    marginTop: 0,
+  },
+
+  container3_profile:{
+    alignItems: 'left',
+    justifyContent: 'center',
+    height: 100,
+    backgroundColor: '#FAEFF1',
+    marginTop: -70,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+
+  container4_profile: {
+    flexDirection: 'row', 
+    backgroundColor: '#FAEFF1',
+    height: 135,
+    marginLeft: 40,
+    marginRight: 40,
+    marginTop: 30,
+  },
+  column: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+
+  container5_profile:{
+    height: 180,
+    backgroundColor: '#FAEFF1',
+    marginLeft: 40,
+    marginRight: 40,
+  },
+
+  container6_profile:{
+    alignSelf: 'flex-start',
+    padding: 10,
+    backgroundColor: '#D9D9D9',
+    borderRadius: 20,
+    marginTop: 4,
+  },
+
+  circle_profile: {
+    width: 130, 
+    height: 130, 
+    backgroundColor: '#D88393', 
+    borderRadius: 65, 
+    alignItem: 'center', 
+    justifyContent: 'center',
+
+  },
   text: {
     color: '#FAEFF1',
     fontSize: 12,
   },
+
+  text_header1: {
+    fontSize: 25, 
+    fontFamily: 'sans-serif-light', 
+    fontWeight: 'bold',
+    color: '#000000', 
+    textAlign: 'center', 
+    alignSelf: 'flex', 
+  },
+
+  text_header2: {
+    fontSize: 15, 
+    fontFamily: 'sans-serif-thin', 
+    fontWeight: 'bold', 
+    color: '#000000', 
+    textAlign: 'center', 
+    alignSelf: 'flex', 
+  },
+
+  text_header3: {
+    fontSize: 18, 
+    fontFamily: 'sans-serif-light', 
+    fontWeight: 'bold', 
+    color: '#000000', 
+    textAlign: 'left', 
+    alignSelf: 'flex-start', 
+    marginTop: 12, 
+    marginBottom: 6,
+  },
+
+  text_header4: {
+    fontSize: 13, 
+    fontFamily: 'sans-serif-thin', 
+    fontWeight: 'bold', 
+    color: '#000000',
+    textAlign: 'left', 
+    alignSelf: 'flex-start', 
+    marginTop: 0,
+  },
+
+  text_header5: {
+    fontSize: 13, 
+    fontFamily: 'sans-serif-thin', 
+    fontWeight: 'bold', 
+    color: '#000000', 
+    textAlign: 'left', 
+    alignSelf: 'flex-start', 
+    marginTop: -4,
+  },
+
   button_text: {
     color: '#DA4167',
     fontSize: 16,
